127549d04e32c5dc9c87a4508d742ac2b3dc434e
山下大貴 <yamashita.taiki@garoop-company.com>
2025-09-05 11:14:19 +0900

    広告

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7714651880162273"
+          crossOrigin="anonymous"></script>
+      </head>
       <body className="antialiased">
         {children}
       </body>
